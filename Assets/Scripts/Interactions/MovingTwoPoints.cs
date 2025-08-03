29404deb6974770c816d9c669d951ab3bb038ebb
Caramel-Pudding <kuznetsov.eric@gmail.com>
2025-08-03 18:35:16 +0200

    demo walk

---
+++ Assets/Scripts/Interactions/MovingTwoPoints.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SawTrapMover : MonoBehaviour
+{
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private float speed = 2f;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = pointB;
+    }
+
+    void Update()
+    {
+        // Движемся к цели
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target.position,
+            speed * Time.deltaTime
+        );
+
+        // Если практически достигли цели — ставим позицию ровно и меняем target
+        if (Vector3.Distance(transform.position, target.position) < 0.01f)
+        {
+            transform.position = target.position;
+            target = target == pointA ? pointB : pointA;
+        }
+    }
+
+}
